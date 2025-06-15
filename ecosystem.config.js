2594c7e873c5a4a74de1fc6d22e194f5fe7c0ad3
Stone <syj88668@gmail.com>
2025-06-15 12:08:28 +0000

    [feat] - add google info

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "prompt-optimizer",
       script: "serve",
-      args: "-s packages/web/dist -l 3000 --single",
+      args: "-s /home/syj88668/display/prompt-optimizer/current -l 3000 --single",
       env: {
         NODE_ENV: "production",
       },
